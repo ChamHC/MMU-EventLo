6945a795cb5e448cfb2a41767be3088734abe8f3
ChamHC <chcham2207@gmail.com>
2024-06-14 10:07:59 +0800

    Add Role Manager

--- manager.php
+++ manager.php
@@ -1,3 +1,66 @@
+<?php
+    function displayNews(){
+        echo ("
+            <div class='content-block'>
+                <h2>eBwise is replacing MMLS</h2><hr>
+                <div class='content-main'>
+                <p id='Description'>
+                    Dear MMU Students, <br><br>
+                    We are excited to announce that MMU is transitioning from the existing Learning Management System (LMS), MMLS to a new
+                    Moodle-based LMS called eBwise. This transition marks a significant upgrade in our technological infrastructure to enhance your learning experience.
+                </p> 
+                <div class='content-bottom'>
+                    <p id='ContentDetails'>
+                        Date: 15th March 2024<br>
+                        Posted by PROF. DR WONG HIN YONG
+                    </p>
+                    <div class='button-container'>
+                        <img id='EditButton' src='images/edit.png' alt='Edit'>
+                        <img id='DeleteButton' src='images/delete.png' alt='Delete'>
+                    </div>
+                </div>
+                </div>
+            </div>
+        ");
+    }
+
+    function displayRole(){
+        echo ("
+            <div class='content-block'>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Student ID</th>
+                        <th>Contact Number</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>Cham Hao Cheng</td>
+                        <td>1211304951</td>
+                        <td>0123456789</td>
+                        <td>1211304951@student.mmu.edu.my</td>
+                        <td>
+                            <button id='ApproveButton'>Approve</button>
+                            <button id='RejectButton'>Reject</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Cham Hao Cheng</td>
+                        <td>1211304951</td>
+                        <td>0123456789</td>
+                        <td>1211304951@student.mmu.edu.my</td>
+                        <td>
+                            <button id='ApproveButton'>Approve</button>
+                            <button id='RejectButton'>Reject</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        ");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,52 +81,52 @@
     </div>
     <div class="main">
         <div class="buttons">
-            <button id="NewsButton" onclick="">News</button>
-            <button id="RoleButton" onclick="">Role</button>
-            <button id="EventButton" onclick="">Event</button>
-            <button id="FAQButton" onclick="">FAQ</button>
+            <button id="NewsButton">News</button>
+            <button id="RoleButton">Role</button>
+            <button id="EventButton">Event</button>
+            <button id="FAQButton">FAQ</button>
+            <script>
+                document.getElementById("NewsButton").onclick = function(){
+                    window.location.href = "manager.php?page=newsManager";
+                }
+                document.getElementById("RoleButton").onclick = function(){
+                    window.location.href = "manager.php?page=roleManager";
+                }
+                document.getElementById("EventButton").onclick = function(){
+                    window.location.href = "manager.php?page=eventManager";
+                }
+                document.getElementById("FAQButton").onclick = function(){
+                    window.location.href = "manager.php?page=faqManager";
+                }
+            </script>
+        </div>
         </div>
         <div class="content">
-            <div class="content-block">
-                <h2>eBwise is replacing MMLS</h2><hr>
-                <div class="content-main">
-                <p id="Description">
-                    Dear MMU Students, <br><br>
-                    We are excited to announce that MMU is transitioning from the existing Learning Management System (LMS), MMLS to a new
-                    Moodle-based LMS called eBwise. This transition marks a significant upgrade in our technological infrastructure to enhance your learning experience.
-                </p> 
-                <div class="content-bottom">
-                    <p id="ContentDetails">
-                        Date: 15th March 2024<br>
-                        Posted by PROF. DR WONG HIN YONG
-                    </p>
-                    <div class="button-container">
-                        <img id="EditButton" src = "images/edit.png" alt="Edit">
-                        <img id="DeleteButton" src = "images/delete.png" alt="Delete">
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="content-block">
-                <h2>eBwise is replacing MMLS</h2><hr>
-                <div class="content-main">
-                <p id="Description">
-                    Dear MMU Students, <br><br>
-                    We are excited to announce that MMU is transitioning from the existing Learning Management System (LMS), MMLS to a new
-                    Moodle-based LMS called eBwise. This transition marks a significant upgrade in our technological infrastructure to enhance your learning experience.
-                </p> 
-                <div class="content-bottom">
-                    <p id="ContentDetails">
-                        Date: 15th March 2024<br>
-                        Posted by PROF. DR WONG HIN YONG
-                    </p>
-                    <div class="button-container">
-                        <img id="EditButton" src = "images/edit.png" alt="Edit">
-                        <img id="DeleteButton" src = "images/delete.png" alt="Delete">
-                    </div>
-                </div>
-                </div>
-            </div>
+            <?php
+                if(isset($_GET['page'])){
+                    switch ($_GET['page']) {
+                        case 'newsManager':
+                            displayNews();
+                            break;
+                        case 'roleManager':
+                            displayRole();
+                            break;
+                        case 'eventManager':
+                            echo 'eventManager';
+                            break;
+                        case 'faqManager':
+                            echo 'faqManager';
+                            break;
+                        default:
+                            echo 
+                            "<script>
+                                alert('Invalid Page.');
+                                window.location.href = 'manager.php?page=newsManager';
+                            </script>";
+                            break;
+                    }
+                }
+            ?>
             <img id="Add" src="images/add.png" alt="Add">
         </div>
     </div>
