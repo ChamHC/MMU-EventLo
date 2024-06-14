4cad673a86023b9d1d9ebbc0dcd0d92abb26233f
ChamHC <chcham2207@gmail.com>
2024-06-14 11:14:30 +0800

    Add Navigation Highlight

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
         <a href="index.html"><img src="images/logo.png" alt="MMU Event Organizer" class="logo"></a>
         <nav class="main-nav">
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="catalogue.html">E-Catalogue</a></li>
-                <li><a href="events.php">Events</a></li>
-                <li><a href="faqs.html">FAQs</a></li>
-                <li><a href="aboutUs.html">About Us</a></li>
+                <li><a href="index.html" id="Home">Home</a></li>
+                <li><a href="catalogue.html" id="ECatalogue">E-Catalogue</a></li>
+                <li><a href="events.php" id="Events">Events</a></li>
+                <li><a href="faqs.html" id="FAQs">FAQs</a></li>
+                <li><a href="aboutUs.html" id="AboutUs">About Us</a></li>
             </ul>
         </nav>
         <img src="images/profile.png" alt="Profile" class="profile-icon" onclick="">
@@ -22,4 +22,26 @@
         </header>
         '
     );
+
+    $includedFile = basename($_SERVER['PHP_SELF']);
+    switch ($includedFile) {
+        case 'events.php':
+            setColor('Events');
+            break;
+        case 'manager.php':
+            setColor('Home');
+            break;
+        default:
+            setColor('Home');
+            break;
+    }
+
+    function setColor($id){
+        echo ("
+            <script>
+                var link = document.getElementById('$id');
+                link.style.color = 'white';
+            </script>
+        ");
+    }
 ?>
